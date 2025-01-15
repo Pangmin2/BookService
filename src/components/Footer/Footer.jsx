6853eaa8a6cfd98f3e9ccaf9b035ca445050f107
yunadada <ynson0216@naver.com>
2025-01-15 16:43:14 +0900

    feat: 공통 Footer 컴포넌트 UI 구현

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.content}>
+        <p>Related</p>
+        <p>국립금오공과대학교 컴퓨터공학부</p>
+        <p>@ CHIP_SAT Page 2024</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
