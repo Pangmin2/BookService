377fa4d659e4c953b9a6c81cb345b8e8f37cd434
yunadada <ynson0216@naver.com>
2025-01-18 17:42:27 +0900

    feat: 도서 검색 메인 페이지와 도서 상세보기 페이지 분리

---
+++ src/pages/SearchBook/SearchBook.jsx
@@ -0,0 +1,17 @@
+import Header from "../../components/Header/Header";
+import Hero from "../../components/Hero/Hero";
+import Footer from "../../components/Footer/Footer";
+import Book from "../../components/Book/Book";
+
+const SearchBook = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Book />
+      <Footer />
+    </>
+  );
+};
+
+export default SearchBook;
