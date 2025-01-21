77bbe5ec467331f7b8defb64a98044912571dcf5
yunadada <ynson0216@naver.com>
2025-01-21 17:39:46 +0900

    feat: 도서 상세 정보 렌더링 기능 구현 및 예약 상태에 따른 버튼 활성화 기능 구현

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,27 +1,51 @@
+import { useLocation } from "react-router-dom";
 import style from "./BookDetails.module.css";
+
 const BookDetails = () => {
+  const location = useLocation();
+  const { book } = location.state || {}; // 전달된 book 데이터
+
+  const bookStatus = {
+    AVAILABLE: "대여 가능",
+    RESERVED: "예약중",
+    FULLY_RESERVED: "예약 불가",
+  };
+
+  const requestReservation = () => {
+    alert("예약 신청 성공");
+  };
+
   return (
     <div className={style.container}>
-      <h2>HTTP 완벽 가이드</h2>
+      <h2>{book.title}</h2>
       <hr />
       <div className={style.contents}>
         <div className={style.bookImage}>
-          <img src="https://picsum.photos/200"></img>
+          <img
+            src={book.bookUrl || "https://picsum.photos/200"}
+            alt={book.title}
+          ></img>
         </div>
         <div className={style.bookDetails}>
           <ul>
-            <li>제목: 어쩌고저쩌고</li>
-            <li>저자: 어쩌고저쩌고</li>
-            <li>발행처: 어쩌고저쩌고</li>
-            <li>발행년: 어쩌고저쩌고</li>
-            <li>상태: 대여가능 or 예약중(2)</li>
-            <li>예약: 2명</li>
+            <li>제목: {book.title}</li>
+            <li>저자: {book.author}</li>
+            <li>발행처: {book.publisher}</li>
+            <li>발행년: {book.publishYear}</li>
+            <li>상태: {bookStatus[book.status]}</li>
+            <li>예약: {book.borrowCount}명</li>
           </ul>
         </div>
       </div>
       <hr />
       <div className={style.button}>
-        <button>예약</button>
+        <button
+          disabled={book.borrowCount >= 3}
+          className={book.borrowCount >= 3 ? style.disableButton : style.button}
+          onClick={requestReservation}
+        >
+          예약
+        </button>
       </div>
     </div>
   );
