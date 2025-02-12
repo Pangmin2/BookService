2d7227b1a4ed7c45d072558dc8d0d691bf7d7e7a
yunadada <ynson0216@naver.com>
2025-02-12 20:26:44 +0900

    feat: 로그아웃 기능 구현

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,29 +2,33 @@ import { Link } from "react-router-dom";
 import style from "./Header.module.css";
 import { FaBars } from "react-icons/fa";
 import { useState } from "react";
+import useUserStore from "../../../store/useUserStore";
 
 const Header = () => {
   const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const isLogined = useUserStore((state) => state.isLogined);
+  const setIsLogined = useUserStore((state) => state.setIsLogined);
+
+  const onLogout = () => {
+    setIsLogined(false);
+    localStorage.removeItem("accessToken");
+    alert("로그아웃 되었습니다.");
+  };
 
   return (
-    <header className={style.header}>
+    <header>
       <div className={style.contents}>
-        <div 
-          className={style.bars}
-          onMouseEnter={() => setIsMenuVisible(true)}
-        >
+        <div className={style.bars} onMouseEnter={() => setIsMenuVisible(true)}>
           <FaBars />
         </div>
         {isMenuVisible && (
-          <div 
+          <div
             className={style.sideMenu}
             onMouseLeave={() => setIsMenuVisible(false)}
             onMouseEnter={() => setIsMenuVisible(true)}
           >
             <div className={style.profileSection}>
-              <div className={style.profileImage}>
-                {/* 프로필 이미지 */}
-              </div>
+              <div className={style.profileImage}>{/* 프로필 이미지 */}</div>
               <div className={style.profileInfo}>
                 {/* 프로필 정보, 추후 수정해야함 */}
                 <h3>관리자</h3>
@@ -32,15 +36,23 @@ const Header = () => {
               </div>
             </div>
             <ul>
-              <li><a href="/bookassign">도서 등록</a></li>
-              <li><a href="/membermanage">부원 관리</a></li>
+              <li>
+                <a href="/bookassign">도서 등록</a>
+              </li>
+              <li>
+                <a href="/membermanage">부원 관리</a>
+              </li>
             </ul>
           </div>
         )}
         <nav className={style.navigation}>
           <ul>
-          <li>
-              <Link to="/login">로그인</Link>
+            <li>
+              {isLogined ? (
+                <button onClick={onLogout}>로그아웃</button>
+              ) : (
+                <Link to="/login">로그인</Link>
+              )}
             </li>
             <li>
               <div className="verticalLine"> </div>
