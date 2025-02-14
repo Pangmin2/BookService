0edb8504e1f0fbfde1bf6546254f8588587cfb58
yunadada <ynson0216@naver.com>
2025-02-14 17:05:29 +0900

    feat: 전달받은 도서 id를 바탕으로 API 요청 및 디버깅을 위한 임시 테스트 코드 추가

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,23 +1,36 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import style from "./BookDetails.module.css";
 import { useEffect, useState } from "react";
 import { requestWithAuth } from "../../utils/requestWithAuth";
 
-const BookDetails = ({ book }) => {
+const BookDetails = () => {
+  const location = useLocation();
+  const id = location.state.id;
   const [canCancel, setCanCancel] = useState(false);
-  const navigate = useNavigate();
+  const [book, setBook] = useState(null);
 
   useEffect(() => {
-    if (!book || Object.keys(book).length === 0) {
-      console.error("책 정보가 없습니다. 홈으로 이동합니다.");
-      alert("잘못된 접근입니다. 홈으로 이동합니다.");
-      navigate("/"); // 올바른 데이터가 없을 경우 홈으로 이동
-    }
-  }, [book, navigate]);
+    console.log("마운트");
+    console.log("전달된 id값", id);
+  }, []);
 
-  // if (!book || Object.keys(book).length === 0) {
-  //   return null; // 리디렉션 처리 중 화면에 아무것도 표시하지 않음
-  // }
+  useEffect(() => {
+    const fetchBookDetails = async () => {
+      try {
+        console.log("요청하는 URL:", `/books/${id}`);
+        const response = await requestWithAuth("GET", `/books/${id}`);
+        console.log(response);
+        if (response.success) {
+          setBook(response.data);
+        }
+      } catch (error) {
+        alert("도서 정보를 가져오는 데 실패했습니다.");
+        console.error("도서 정보를 가져오는 데 실패했습니다.", error);
+      }
+    };
+
+    if (id) fetchBookDetails();
+  }, [id]);
 
   const bookStatus = {
     AVAILABLE: "대여 가능",
@@ -36,6 +49,7 @@ const BookDetails = ({ book }) => {
       if (response.success) {
         alert("예약 신청 성공");
         setCanCancel(true);
+        //TODO 예약 신청하면 재렌더링 돼서 인원바로 변경되도록
         console.log(response.data);
         return true;
       }
@@ -86,23 +100,23 @@ const BookDetails = ({ book }) => {
   return (
     <div className={style.wrapper}>
       <div className={style.container}>
-        <h2>{book.title}</h2>
+        <h2>{book?.title}</h2>
         <hr />
         <div className={style.contents}>
           <div className={style.bookImage}>
             <img
-              src={book.bookUrl || "https://picsum.photos/200"}
-              alt={book.title}
+              src={book?.bookUrl || "https://picsum.photos/200"}
+              alt={book?.title}
             ></img>
           </div>
           <div className={style.bookDetails}>
             <ul>
-              <li>제목: {book.title}</li>
-              <li>저자: {book.author}</li>
-              <li>발행처: {book.publisher}</li>
-              <li>발행년: {book.publishYear}</li>
-              <li>상태: {bookStatus[book.status]}</li>
-              <li>예약: {book.borrowCount}명</li>
+              <li>제목: {book?.title}</li>
+              <li>저자: {book?.author}</li>
+              <li>발행처: {book?.publisher}</li>
+              <li>발행년: {book?.publishYear}</li>
+              <li>상태: {bookStatus[book?.status]}</li>
+              <li>예약: {book?.borrowCount}명</li>
             </ul>
           </div>
         </div>
@@ -116,9 +130,9 @@ const BookDetails = ({ book }) => {
             취소
           </button>
           <button
-            disabled={book.borrowCount >= 3}
+            disabled={book?.borrowCount >= 3}
             className={
-              book.borrowCount >= 3 ? style.disableButton : style.button
+              book?.borrowCount >= 3 ? style.disableButton : style.button
             }
             onClick={requestReservation}
           >
