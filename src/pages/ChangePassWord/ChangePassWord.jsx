f3fd754c305214523a14f61704b34e215e456b3e
yunadada <ynson0216@naver.com>
2025-02-21 18:09:32 +0900

    feat: 비밀번호 변경 페이지 추가

---
+++ src/pages/ChangePassWord/ChangePassWord.jsx
@@ -0,0 +1,11 @@
+import Layout from "../../components/Layout/Layout";
+
+const ChangePassWord = () => {
+  return (
+    <>
+      <Layout content={<p>비밀번호 변경</p>} />
+    </>
+  );
+};
+
+export default ChangePassWord;
