bd142f4b45de1ecae5585b4f168dd59ef0797b41
yunadada <ynson0216@naver.com>
2025-03-01 18:23:32 +0900

    refactor: 도서 상세 보기 비즈니스 로직을 컴포넌트에서 페이지로 이동

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,111 +1,14 @@
-import { useLocation } from "react-router-dom";
 import style from "./BookDetails.module.css";
-import { useEffect, useState } from "react";
-import { requestWithAuth } from "../../utils/requestWithAuth";
 import useBookStore from "../../../store/useBookStore";
 
-const BookDetails = () => {
-  const location = useLocation();
-  const id = location.state.id;
-  const [book, setBook] = useState(null);
-  const { addReservation, removeReservation, isReserved } = useBookStore();
-  const canCancel = isReserved(id); // 현재 책이 예약된 상태인지 확인
-
-  useEffect(() => {
-    console.log("마운트");
-    console.log("전달된 id값", id);
-  }, []);
-
-  useEffect(() => {
-    const fetchBookDetails = async () => {
-      try {
-        console.log("요청하는 URL:", `/books/${id}`);
-        const response = await requestWithAuth("GET", `/books/${id}`);
-        console.log(response);
-        if (response.success) {
-          setBook(response.data);
-        }
-      } catch (error) {
-        alert("도서 정보를 가져오는 데 실패했습니다.");
-        console.error("도서 정보를 가져오는 데 실패했습니다.", error);
-      }
-    };
-
-    if (id) fetchBookDetails();
-  }, [id]);
-
+const BookDetails = ({ book, requestReservation, requestCancel }) => {
+  const { isReserved } = useBookStore();
+  const canCancel = isReserved(book.id); // 현재 책이 예약된 상태인지 확인
+  console.log(book);
   const bookStatus = {
     AVAILABLE: "대여 가능",
     RESERVED: "예약중",
     FULLY_RESERVED: "예약 불가",
-  };
-
-  const requestReservation = async () => {
-    try {
-      const response = await requestWithAuth(
-        "POST",
-        `/books/reservation/${book?.id}`
-      );
-      console.log("응답: ", response);
-      console.log(response.success);
-      if (response.success) {
-        alert("예약 신청 성공");
-        addReservation(book?.id);
-        //TODO 예약 신청하면 재렌더링 돼서 인원바로 변경되도록
-        setBook((prevBook) => ({
-          ...prevBook,
-          borrowCount: prevBook.borrowCount + 1, // 예약 인원 증가
-        }));
-        console.log(response.data);
-        return true;
-      }
-    } catch (error) {
-      console.log("현재 에러:", error);
-      switch (error.code) {
-        case "B002":
-          alert("현재 예약 가능한 인원이 모두 찼습니다.");
-          console.error(error.msg);
-          return null;
-        case "B007":
-          alert("이미 예약 신청중인 도서입니다.");
-          console.error(error.msg);
-          return null;
-        default:
-          alert("예약 신청 실패");
-          console.error("알 수 없는 에러러러: ", error);
-          return null;
-      }
-    }
-  };
-
-  const requestCancel = async () => {
-    //책 id와 토큰 보내기
-    try {
-      const response = await requestWithAuth(
-        "DELETE",
-        `/books/reservation/${book?.id}`
-      );
-      console.log("취소:", response);
-      if (response.success) {
-        alert("예약 취소 요청 성공");
-        removeReservation(book?.id);
-        setBook((prevBook) => ({
-          ...prevBook,
-          borrowCount: prevBook.borrowCount - 1, // 예약 인원 감소
-        }));
-        console.log(response.data);
-        return true;
-      }
-    } catch (error) {
-      console.log("예약 취소 관련 에러", error);
-      if (error.code === "B003") {
-        alert(error.msg);
-        console.warn(error.msg);
-      } else {
-        alert("예약 취소 실패");
-        console.warn(error);
-      }
-    }
   };
 
   return (
