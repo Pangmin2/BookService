f0ec20d16703f48ccf3efca3667cea8a5b7fd855
yunadada <ynson0216@naver.com>
2025-03-11 19:59:20 +0900

    chore: 존재하지 않는 파일 제거

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import Login from "./pages/Login/Login_Page";
 import SignUp from "./pages/SignUp/SignUp_Page";
 import BookInfo from "./pages/BookInfo/BookInfo";
 import Findform from "./pages/Findform/Findform";
-import MyBookPage from "./pages/MyPage/MyBookPage";
 import BookEdit from "./pages/Administrator/BookEdit/BookEdit";
 import MyPage from "./pages/MyPage/MyPage";
 
@@ -28,7 +27,6 @@ function App() {
         <Route path="/member_manage" element={<MemberManage />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/findform" element={<Findform />} />
-        <Route path="/my_book_page" element={<MyBookPage />} />
         <Route path="/book_edit" element={<BookEdit />} />
       </Routes>
     </Router>
