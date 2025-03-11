60bf33e61460d25d296d6e9a957c87279a2b9ada
yunadada <ynson0216@naver.com>
2025-03-11 14:32:35 +0900

    fix: 로그인 상태에 따라 콘텐츠 컴포넌트 조건부 렌더링

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,25 @@
 import style from "./Layout.module.css";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
-import Hero from "../Hero/Hero";
+import useUserStore from "../../../store/useUserStore";
+import GuestBlock from "../GuestBlock/GuestBlock";
 
 const Layout = ({ hero, content }) => {
+  const isLogined = useUserStore((state) => state.isLogined);
+
   return (
     <div className={style.wrapper}>
       <Header />
-      {hero && hero}
-      <div className={style.main}>{content}</div>
+
+      {isLogined ? (
+        <>
+          {hero && hero}
+          <div className={style.main}>{content}</div>
+        </>
+      ) : (
+        <GuestBlock />
+      )}
+
       <Footer />
     </div>
   );
