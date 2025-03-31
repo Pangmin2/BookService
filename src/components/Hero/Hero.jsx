bd9133b76d5d85c62a93cfddd31cc3c13e169abb
Pangmin2 <totoro7378@naver.com>
2025-03-31 16:53:47 +0900

    fix: 검색창 및 상세페이지 모바일 UI 개선

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import style from "./Hero.module.css";
+import { useIsMobile } from '../../hooks/useIsMobile';
 
 const Hero = ({ section, onSearch }) => {
   const [searchTitle, setSearchTitle] = useState("");
+  const isMobile = useIsMobile();
 
   const searchHandler = () => {
     onSearch(searchTitle);
@@ -10,17 +12,18 @@ const Hero = ({ section, onSearch }) => {
 
   return (
     <section>
-      <div className={style.contents}>
+      <div className={`${style.contents} ${isMobile ? style.mobileContainer : ''}`}>
         <h1>{section || "제목"}</h1>
         {onSearch && (
-          <div className={style.search}>
+          <div className={`${style.search} ${isMobile ? style.mobileSearchForm : ''}`}>
             <input
+              className={isMobile ? style.mobileInput : ''}
               placeholder="검색어를 입력하세요."
               value={searchTitle}
               onChange={(e) => {
                 setSearchTitle(e.target.value);
               }}
-            ></input>
+            />
             <button onClick={searchHandler}>검색</button>
           </div>
         )}
